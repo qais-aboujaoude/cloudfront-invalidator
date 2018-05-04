ef94a750d35095c7e0c4493f97123fea94208fbe
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-04 20:28:10 +0300

    Add listDistibutions method

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const util = require('util')
 const aws = require('aws-sdk')
       cloudfront = new aws.CloudFront()
+aws.config.setPromisesDependency();
 
-cloudfront.listDistributions({}, function (err, data) {
-  err ? console.log(err) : console.log(util.inspect(data, {showHidden: false, depth: null}))
-})
-
+const listDistributions = () => (
+  new Promise((resolve, reject) => {
+    cloudfront.listDistributions({}, (err, data) => {
+      err ? reject(err)
+          : resolve(data)
+    })
+  })
+)
+listDistributions()
+  //.then(r => console.log(util.inspect(r, {showHidden: false, depth: null}))
+  .then(r => console.log(r))
+  .catch(e => console.log(e))
 
