bfb987a559d92d723ac393e92be45e5adcc0086c
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-08 19:42:17 +0300

    Add createInvalidation function call

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const aws = require('aws-sdk'),
       log = require('./utils'),
       cloudfront = new aws.CloudFront()
+
 const dist = 'haulo-driver-client-master'
 
 const listDistributions = () => (
   new Promise((resolve, reject) => {
     cloudfront.listDistributions({}, (err, data) => {
-      err ? reject(err)
-          : resolve(data)
+      err ? reject(err) : resolve(data)
     })
   })
 )
+
 listDistributions()
   .then(r => {
-    const m = r.DistributionList.Items.map(e => e.Origins.Items)
-    const x = m.map(e => e.map(i => i.Id.substring(3)))
-    log(x[0])
-    const kousa = [].concat.apply([], x)
-    log(kousa)
-    console.log(kousa.includes(dist))
-    console.log(kousa.findIndex(i => i === dist))
-    console.log(r.DistributionList.Items[0].Id)
+    const distributionItems = r.DistributionList.Items.map(e => e.Origins.Items)
+    //const listOfOrigins = distributionItems.map(e => e.map(i => i.Id.substring(3)))
+    //const kousa = [].concat.apply([], listOfOrigins)
+    const listOfOrigins = [].concat.apply(
+      [], distributionItems.map(e => e.map(i => i.Id.substring(3)))
+    )
+    //console.log(kousa.findIndex(i => i === dist))
+    const distributionId = r.DistributionList.Items[listOfOrigins.findIndex(i => i === dist)].Id
+    log(distributionId)
+
+    cloudfront.createInvalidation({
+      DistributionId: distributionId,
+      InvalidationBatch: { 
+        CallerReference: Date.now().toString(), 
+        Paths: { 
+          Quantity: 1, 
+          Items: [
+            '/*',
+          ]
+        }
+      }
+    }, (err, data) => {
+      err ? console.log(err, err.stack)
+          : console.log(data)
+    })
+
   })
   .catch(e => log(e))
 
