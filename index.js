473c2211d4ef29857b29b433ace9636126e39204
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-13 18:21:29 +0300

    Add command line arguments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const aws = require('aws-sdk'),
   cloudfront = new aws.CloudFront()
 
+const arg  = process.argv.slice(2)[0];
+
 const listDistributions = () => (
   new Promise((resolve, reject) => {
     cloudfront.listDistributions({}, (err, data) => {
@@ -9,25 +11,27 @@ const listDistributions = () => (
   })
 )
 
-listDistributions()
-  .then(r => {
-    const distributionItems = r.DistributionList.Items.map(e => e.Origins.Items)
-    const listOfOrigins = [].concat.apply([],
-      distributionItems.map(e => e.map(i => i.Id.substring(3))))
-    const distributionId = r.DistributionList.Items[listOfOrigins.findIndex(i => i === PARAM)].Id
-    cloudfront.createInvalidation({
-      DistributionId: distributionId,
-      InvalidationBatch: {
-        CallerReference: Date.now().toString(),
-        Paths: {
-          Quantity: 1,
-          Items: ['/*']
+arg
+  ? listDistributions()
+    .then(r => {
+      const distributionItems = r.DistributionList.Items.map(e => e.Origins.Items)
+      const listOfOrigins = [].concat.apply([],
+        distributionItems.map(e => e.map(i => i.Id.substring(3))))
+      const distributionId = r.DistributionList.Items[listOfOrigins.findIndex(i => i === arg)].Id
+      cloudfront.createInvalidation({
+        DistributionId: distributionId,
+        InvalidationBatch: {
+          CallerReference: Date.now().toString(),
+          Paths: {
+            Quantity: 1,
+            Items: ['/*']
+          }
         }
-      }
-    }, (err, data) => {
-      err ? console.log(err)
-          : console.log(data)
+      }, (err, data) => {
+        err ? console.log(err)
+            : console.log(data)
+      })
     })
-  })
-  .catch(e => console.log(e))
+    .catch(e => console.log(e))
+  : console.log('no argument provided')
 
