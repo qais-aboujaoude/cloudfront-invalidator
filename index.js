4a0f49d9cfd2054abddbdf5e7a6962120673d803
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-13 16:58:39 +0300

    Remove export handler and lambda logic

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const aws = require('aws-sdk'),
-      putJob = require('./putJob'),
-      cloudfront = new aws.CloudFront()
+  cloudfront = new aws.CloudFront()
 
 const listDistributions = () => (
   new Promise((resolve, reject) => {
@@ -10,29 +9,27 @@ const listDistributions = () => (
   })
 )
 
-exports.handler = (event, context) => {
-  const params =  JSON.parse(event["CodePipeline.job"].data.actionConfiguration
-                  .configuration.UserParameters)
-  listDistributions()
-    .then(r => {
-      const distributionItems = r.DistributionList.Items.map(e => e.Origins.Items)
-      const listOfOrigins = [].concat.apply([],
-        distributionItems.map(e => e.map(i => i.Id.substring(3))))
-      const distributionId = r.DistributionList.Items[listOfOrigins.findIndex(i => i === params.S3Bucket)].Id
-      cloudfront.createInvalidation({
-        DistributionId: distributionId,
-        InvalidationBatch: {
-          CallerReference: Date.now().toString(),
-          Paths: {
-            Quantity: 1,
-            Items: ['/*']
-          }
+const params =  JSON.parse(event["CodePipeline.job"].data.actionConfiguration
+  .configuration.UserParameters)
+listDistributions()
+  .then(r => {
+    const distributionItems = r.DistributionList.Items.map(e => e.Origins.Items)
+    const listOfOrigins = [].concat.apply([],
+      distributionItems.map(e => e.map(i => i.Id.substring(3))))
+    const distributionId = r.DistributionList.Items[listOfOrigins.findIndex(i => i === params.S3Bucket)].Id
+    cloudfront.createInvalidation({
+      DistributionId: distributionId,
+      InvalidationBatch: {
+        CallerReference: Date.now().toString(),
+        Paths: {
+          Quantity: 1,
+          Items: ['/*']
         }
-      }, (err, data) => {
-        err ? putJob.failure(event["CodePipeline.job"].id, context.invokeid, err)
-            : putJob.success(event["CodePipeline.job"].id,'Success: invalidated')
-      })
+      }
+    }, (err, data) => {
+      err ? putJob.failure(event["CodePipeline.job"].id, context.invokeid, err)
+        : putJob.success(event["CodePipeline.job"].id,'Success: invalidated')
     })
-    .catch(e => putJob.failure(event["CodePipeline.job"].id, context.invokeid, e))
-}
+  })
+  .catch(e => putJob.failure(event["CodePipeline.job"].id, context.invokeid, e))
 
