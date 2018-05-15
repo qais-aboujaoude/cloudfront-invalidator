560d9d476549e25ea83eecb0ea78a3a374e96802
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-15 15:04:31 +0300

    Refactor call cloudfront

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
-const util = require('util')
-const log = o => console.log(util.inspect(o, false, null))
-
 const cloudfront = require('./src/cloudfront')
+
 const distributionItems = list => list.DistributionList.Items.map(e => e.Origins.Items)
 
-cloudfront.listDistributions()
-  .then(r => {
-    log(distributionItems(r))
-  })
-  .catch(e => log(e))
-
+// cloudfront.invalidateBucket('haulo-admin-client-testing')
