fc7c87d82df7dba60a1742e97caa50c46d2a1840
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-16 16:28:47 +0300

    Add Documentation for SDK methods

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ program.bucket.length
   ? (() => {
     const buckets = process.argv.splice(3)
     buckets.forEach(async e => await cloudfront.invalidateBucket((e)))
-  })()
-  : (() => { throw new Error('No S3 Bucket provided') })()
+    })()
+  : (() => { throw new Error('No S3 Bucket(s) provided') })()
 
--- src/cloudfront.js
+++ src/cloudfront.js
@@ -3,6 +3,12 @@ process.env.AWS_SDK_LOAD_CONFIG = true
 const aws        = require('aws-sdk'),
       cloudfront = new aws.CloudFront()
 
+/**
+ * @async
+ * @method listDistributions A wrapper around Cf SDK listDistributions 
+ * @return {Promise<{object}>} returns an object containing information about
+ * all the distributions associated with the user account
+ */
 const listDistributions = () => (
   new Promise((resolve, reject) => {
     cloudfront.listDistributions({}, (err, data) => 
@@ -10,6 +16,14 @@ const listDistributions = () => (
   })
 )
 
+/**
+ * @method getDistributionId takes the object outcome of @method listDistributions
+ * and checks against the bucket name @param bucket
+ * and returns the distributionId ID of that bucket
+ * @param {object} list list distributions object to iterate over
+ * @param {string} bucket name of the bucket to check against
+ * @return {string} Id of the bucket
+ */
 const getDistributionId = (list, bucket) => (
   list.DistributionList.Items[[].concat.apply([],
     list.DistributionList.Items
@@ -17,6 +31,14 @@ const getDistributionId = (list, bucket) => (
       .map(e => e.map(i => i.Id.substring(3)))).findIndex(i => i === bucket)].Id
 )
 
+/**
+ * @async
+ * @method invalidateBucket invalidates the distributions of the bucket 
+ * calls @method listDistributions and @method getDistributionId to retrieve 
+ * the Id and CF SDK @method createInvalidation to create an invalidation
+ * @param {string} bucket name of the bucket to invalidate
+ * @return {Promise<{object}>} data object of @method createInvalidation
+ */
 const invalidateBucket = bucket => (
   new Promise((resolve, reject) => {
     listDistributions()
